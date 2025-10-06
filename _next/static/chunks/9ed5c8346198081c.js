e6c9e8647c471c6d964a1696c098000bfd310dc6
Anton Malkov <74134773+chigart@users.noreply.github.com>
2025-10-06 19:20:35 +0900

    Updates

--- _next/static/chunks/9ed5c8346198081c.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/eca7e4e233d512f2.js",
-  "static/chunks/turbopack-382f7560f3921e5a.js"
-])
--- _next/static/chunks/b42e1654f417c60c.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/eca7e4e233d512f2.js",
-  "static/chunks/turbopack-1405195f07228752.js"
-])
