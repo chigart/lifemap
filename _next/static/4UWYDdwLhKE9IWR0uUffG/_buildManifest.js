696a93d5f9aa6697dccc3ec7ced0d553cff2db68
Anton Malkov <74134773+chigart@users.noreply.github.com>
2025-10-06 19:08:48 +0900

    Updates

---
+++ _next/static/4UWYDdwLhKE9IWR0uUffG/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/b42e1654f417c60c.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
